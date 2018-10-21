95313c8a999eddc45d6c76da23cc29ccf2a9e48c
D. Strekha <mr.choo96@gmail.com>
2018-10-21 13:52:03 +0300

    Remove logic for changing flags

--- bottomsheetmenu/src/main/java/com/strekha/bottomsheetmenu/BaseBottomDialog.kt
+++ bottomsheetmenu/src/main/java/com/strekha/bottomsheetmenu/BaseBottomDialog.kt
@@ -2,13 +2,11 @@ package com.strekha.bottomsheetmenu
 
 import android.content.Context
 import android.content.res.Configuration
-import android.os.Build
 import android.os.Bundle
 import android.support.annotation.StyleRes
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.BottomSheetDialog
 import android.view.ViewGroup
-import android.view.WindowManager
 import android.widget.FrameLayout
 
 class BaseBottomDialog internal constructor(context: Context, @StyleRes theme: Int)
@@ -17,10 +15,6 @@ class BaseBottomDialog internal constructor(context: Context, @StyleRes theme: I
     override fun onCreate(savedInstanceState: Bundle) {
         super.onCreate(savedInstanceState)
         window?.let {
-            if (Build.VERSION.SDK_INT >= 21) {
-                it.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-                it.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            }
             if (!context.resources.getBoolean(R.bool.isTablet)) {
                 it.setLayout(
                         ViewGroup.LayoutParams.MATCH_PARENT,
